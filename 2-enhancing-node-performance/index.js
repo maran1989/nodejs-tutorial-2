fa67b0ea62cddc433ce5d68fa1922d68740afdd7
Mark Anthony Gutierrez <14417@olinkgroup.com>
2018-08-08 06:49:02 +0800

    Modify Section 2

--- 2-enhancing-node-performance/index.js
+++ 2-enhancing-node-performance/index.js
@@ -1,16 +1,15 @@
+const crypto = require('crypto');
 const express = require('express');
 const app = express();
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {
-
-    }
-}
-
 app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hi there');
+    });
+});
+
+app.get('/fast', (req, res) => {
+    res.send('This was fast!');
 });
 
 app.listen(3000);
